1f53574139a1d15acbcf2cdc0d709fc64239d07f
chnbohwr <chnbohwr@gmail.com>
2017-09-03 19:49:50 +0800

    udpate redirecturl

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,6 @@
 export default {
   loginUrl: 'https://access.line.me/dialog/oauth/weblogin',
-  redirectUrl: 'https://d4dd54ac.ngrok.io/le/',
+  redirectUrl: process.env.REDIRECT_URL,
   accessTokenUrl: 'https://api.line.me/v2/oauth/accessToken',
   profileUrl: 'https://api.line.me/v2/profile',
   officialUrl: 'https://www.meowdraw.com.tw/',
