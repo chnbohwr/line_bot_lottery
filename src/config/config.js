b1ef9cdb164aba0dba13d6458c17ff10774517eb
chnbohwr <chnbohwr@gmail.com>
2017-09-18 20:18:14 +0800

    modify official url

--- src/config/config.js
+++ src/config/config.js
@@ -3,7 +3,7 @@ export default {
   redirectUrl: process.env.REDIRECT_URL,
   accessTokenUrl: 'https://api.line.me/v2/oauth/accessToken',
   profileUrl: 'https://api.line.me/v2/profile',
-  officialUrl: 'https://www.meowdraw.com.tw/',
+  officialUrl: 'https://line.me/R/ti/p/%40rrs9919t',
   shareChangeTicketCount: 5, // 5 shares change 1 ticket
   ticketProbability: 5, // 5%
   totalTicket: 200, // total send tickets
