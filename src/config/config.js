65caef728693cf1af98a463d9d18aa19d8430f61
chnbohwr <chnbohwr@gmail.com>
2017-09-23 21:49:38 +0800

    update line bot id

--- src/config/config.js
+++ src/config/config.js
@@ -3,7 +3,7 @@ export default {
   redirectUrl: process.env.REDIRECT_URL,
   accessTokenUrl: 'https://api.line.me/v2/oauth/accessToken',
   profileUrl: 'https://api.line.me/v2/profile',
-  officialUrl: 'https://line.me/R/ti/p/%40rrs9919t',
+  officialUrl: 'https://line.me/R/ti/p/%40yua3575m',
   shareChangeTicketCount: 5, // 5 shares change 1 ticket
   ticketProbability: 5, // 5%
   totalTicket: 200, // total send tickets
