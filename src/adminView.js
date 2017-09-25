d4bf2fe711ac6df8f2bb1bea6f708b4908d79771
chnbohwr <chnbohwr@gmail.com>
2017-09-25 19:23:33 +0800

    update view and share count

--- src/adminView.js
+++ src/adminView.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { Ticket } from './db';
+import { Ticket, Share } from './db';
 import moment from 'moment';
 const adminViewRouter = express.Router();
 const checkLogin = (req, res, next) => {
@@ -11,7 +11,13 @@ const checkLogin = (req, res, next) => {
 };
 adminViewRouter.get('/', checkLogin, async (req, res) => {
   const tickets = await Ticket.find({ bingo: true });
-  res.render('admin/index', { tickets, moment });
+  const daily_report = await Share.aggregate({
+    $group: {
+      _id: { $dateToString: { format: '%Y-%m-%d', date: '$createAt' } },
+      count: { $sum: 1 },
+    },
+  });
+  res.render('admin/index', { tickets, moment, daily_report });
 });
 adminViewRouter.get('/login', (req, res) => res.render('admin/login', { loginInfo: req.flash('loginInfo') }));
 export default adminViewRouter;
